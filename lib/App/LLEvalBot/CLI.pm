bd749df5652166776e7175e96f0f8182a1032aac
Songmu <y.songmu@gmail.com>
2014-05-29 10:54:53 +0900

    add scripts

--- lib/App/LLEvalBot/CLI.pm
+++ lib/App/LLEvalBot/CLI.pm
@@ -11,7 +11,7 @@ use App::LLEvalBot;
 sub run {
     my ($class, @argv) = @_;
 
-    my ($opt, $rest_argv) = $self->parse_options(@argv);
+    my ($opt, $rest_argv) = $class->parse_options(@argv);
     my $bot = App::LLEvalBot->new(config => $opt);
     $bot->run;
 }
@@ -29,15 +29,33 @@ sub parse_options {
         password=s
         channels=s@
         nickname=s
+        enable_ssl
     /) or pod2usage(1);
 
     my @required_options = qw/host channels/;
     pod2usage(2) if grep {!exists $opt{$_}} @required_options;
 
-    $opt{channels} = [ map { split /,/, $_ } @{ $opt{channels} } ];
-    $opt{nickname} //= 'lleval_bot';
+    $opt{channels}      = [ map { split /,/, $_ } @{ $opt{channels} } ];
+    $opt{join_channels} = delete $opt{channels};
+    $opt{nickname}      //= 'lleval_bot';
 
     (\%opt, \@argv);
 }
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::LLEvalBot::CLI - IRC bot for LLeval
+
+=head1 SYNOPSIS
+
+    % lleval-bot.pl --channels=test --host=irc.example.com [opt]
+
+=head1 DESCRIPTION
+
+App::LLEvalBot is IRC bot for LLeval.
--- script/lleval-bot.pl
+++ script/lleval-bot.pl
@@ -3,3 +3,19 @@ use strict;
 use warnings;
 use App::LLEvalBot::CLI;
 App::LLEvalBot::CLI->run(@ARGV);
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+lleval-bot.pl - IRC bot for LLeval
+
+=head1 SYNOPSIS
+
+    % lleval-bot.pl --channels=test --host=irc.example.com [opt]
+
+=head1 DESCRIPTION
+
+App::LLEvalBot is IRC bot for LLeval.
